847be72996164c6b45288b30924418e74d696fb4
ahsankhan201010 <ahsankhan201010@gmail.com>
2021-05-29 16:07:56 +0500

    mongodb atlas connected

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const express = require("express");
+const mongoose = require('mongoose');
+const mongoDB_password = "svPntO9G49WSt97I"
+
+mongoose.connect('mongodb+srv://ahsan:svPntO9G49WSt97I@cluster0.jilja.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true}).then((con) => {
+    console.log("connected to mogndodb")
+    
+})
 
 const app = express();
 
