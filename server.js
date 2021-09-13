4fc34db3583e534af199277e22cee7ba06be2835
ahsankhan201010 <ahsankhan201010@gmail.com>
2021-09-13 23:56:33 +0500

    for testing 1

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 require("dotenv").config();
-const app = require("./app");
-const mongoose = require("mongoose");
+const express = require("express")
+// const app = require("./app");
+const app = express()
+// const mongoose = require("mongoose");
 
-const DB = process.env.MONGO_STRING.replace(
-  "<PASSWORD>",
-  process.env.MONGO_PASSWORD
-);
-mongoose
-  .connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((con) => {
-    console.log("connected to mogndodb");
-  });
+// const DB = process.env.MONGO_STRING.replace(
+//   "<PASSWORD>",
+//   process.env.MONGO_PASSWORD
+// );
+// mongoose
+//   .connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then((con) => {
+//     console.log("connected to mogndodb");
+//   });
 
 app.listen(process.env.PORT, () => {
   console.log("server running on port 8000");
