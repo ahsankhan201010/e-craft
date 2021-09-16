d476deaf166e58196e4c0178d400ea9c372bd9c0
ahsankhan201010 <ahsankhan201010@gmail.com>
2021-09-16 20:00:38 +0500

    stripe webhook test 1

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const orderRouter = require("./routes/orderRoute");
 const rateLimit = require("express-rate-limit"); //for brute force attack
 const mongoSanitize = require("express-mongo-sanitize"); //for noSql query injections
 const xss = require("xss-clean"); //for XSS attack (remove script tags)
-const cors = require('cors')
+const cors = require('cors');
+const { stripeWebhook } = require("./controllers/orderController");
+const bodyParser = require("body-parser")
 
 const limiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 1hr
@@ -23,7 +25,12 @@ app.use(cors({origin: true, credentials: true}))
 app.use(express.static("public"));
 //middlewares
 app.use(limiter);
+
+//stripe webhook
+app.post("/stripe-webhook", bodyParser.raw({type: 'application/json'}), stripeWebhook)
+
 app.use(express.json());
+
 app.use(mongoSanitize());
 app.use(xss());
 
--- controllers/orderController.js
+++ controllers/orderController.js
@@ -44,3 +44,41 @@ exports.generateCheckoutSession = async (req, res) => {
     console.log(error);
   }
 };
+
+exports.stripeWebhook = (request,response) => {
+  const sig = request.headers['stripe-signature'];
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(request.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
+  }
+  catch (err) {
+    console.log(error)
+    response.status(400).send(`Webhook Error: ${err.message}`);
+  }
+
+  console.log(event)
+
+
+  if(event.type === "checkout.session.completed") {
+    console.log("yeah! payment session completed successfully")
+  }
+  // Handle the event
+  // switch (event.type) {
+  //   case 'payment_intent.succeeded':
+  //     const paymentIntent = event.data.object;
+  //     console.log('PaymentIntent was successful!');
+  //     break;
+  //   case 'payment_method.attached':
+  //     const paymentMethod = event.data.object;
+  //     console.log('PaymentMethod was attached to a Customer!');
+  //     break;
+  //   // ... handle other event types
+  //   default:
+  //     console.log(`Unhandled event type ${event.type}`);
+  // }
+
+  // Return a response to acknowledge receipt of the event
+  response.json({received: true});
+}
